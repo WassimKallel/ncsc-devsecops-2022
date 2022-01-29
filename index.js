48679804c241f79db95f506c6bb1a1339603e93f
Amine Troudi <amine@on-board.co>
2022-01-29 11:40:43 +0100

    fix port

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
-const port = const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.statusCode = 200;
