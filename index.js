9865980851bec4f5ce2e14373369ef1a31aa645e
WassimKallel <wassimkallel@think-it.io>
2022-01-29 12:21:26 +0100

    add dynamic analysis

--- index.js
+++ index.js
@@ -2,9 +2,35 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.statusCode = 200;
-  res.send("Hello World!");
+var entries = [];
+app.get("/", (request, response) => {
+  const html = `
+    <html>
+        <body>
+            <form action="/store" method="get">
+                <label>Enter your text</label>
+                <input type="text" name="value"></input>
+                <input type="submit" name="submit"></input>
+            </form>
+            <ul>
+                ${entries.map((value) => "<li>" + value + "</li>")}
+            </ul>
+        </body>
+    </html>
+    `;
+  console.log("getting");
+  response.setHeader("ContentType", "text/html");
+  // response.setHeader("Content-Security-Policy", "default-src 'self'");
+  response.send(html);
+});
+
+app.get("/store", (request, response) => {
+  let body = request.query;
+  const value = body.value;
+  console.log("storing");
+  // entries.push(sanitizer.escape(value));
+  entries.push(value);
+  response.redirect("/");
 });
 
 const server = app.listen(port, () => {
